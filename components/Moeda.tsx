301b90c681469fb62c13f75f0583b1c57bfe93fc
muwaffaq <itzmuwaffaq@gmail.com>
2023-02-05 13:25:25 +0100

    refactored landing page

--- components/Moeda.tsx
+++ components/Moeda.tsx
@@ -10,15 +10,15 @@ const Moeda = ({
   priceChange
 } : any) => {
   return (
-    <div className="max-w-[1500px] mx-auto flex justify-center">
-      <div className="moeda-row flex items-center h-[80px] border-b border-b-[#d7d7d7] md:w-[900px]">
-        <div className="moeda flex items-center pr-[14px] md:pr-[24px] min-w-[150px]">
+    <div className="max-w-[1500px] px-8 mx-auto flex justify-center text-xs md:text-base">
+      <div className="moeda-row flex flex-col md:flex-row md:items-center h-[90px] py-3 border-b border-b-[#d7d7d7]">
+        <div className="moeda flex items-center md:pr-[24px]">
           {/* <img src={image} alt="crypto" /> */}
-          <h2 className="text-base w-[70px] md:w-[150px]">{name}</h2>
+          <h2 className="text-base md:w-[150px]">{name}</h2>
           <p className="moeda-symbol hidden md:block uppercase">{symbol}</p>
         </div>
-        <div className="moeda-data grid gap-2 grid-cols-4 text-right justify-between w-full">
-          <p className="moeda-price md:w-[110px]">${price}</p>
+        <div className="moeda-data py-3 md:py-0 grid gap-4 grid-cols-4 text-right justify-between w-full">
+          <p className="moeda-price  md:w-[110px]">${price}</p>
           <p className="moeda-volume md:w-[155px]">${volume.toLocaleString()}</p>
 
           {priceChange < 0 ? (
@@ -27,7 +27,7 @@ const Moeda = ({
             <p className="moeda-percent  text-green-400">{priceChange.toFixed(2)}%</p>
           )}
 
-          <p className="moeda-marketcap w-[230px]">
+          <p className="moeda-marketcap md:w-[230px]">
             Mkt Cap: ${marketcap.toLocaleString()}
           </p>
         </div>
